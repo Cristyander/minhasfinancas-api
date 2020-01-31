2e0850b7a1b8eab13909f8f23cece594f1e858a6
Cristyander <cristyander@hotmail.com>
2020-01-31 20:19:58 -0300

    Criação de metodos do serviço de lançamentos financeiros

--- src/main/java/com/cristyander/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/cristyander/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -1,11 +1,16 @@
 package com.cristyander.minhasfinancas.service.impl;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cristyander.minhasfinancas.exception.RegraNegocioException;
 import com.cristyander.minhasfinancas.model.entity.Lancamento;
 import com.cristyander.minhasfinancas.model.enums.StatusLancamento;
 import com.cristyander.minhasfinancas.model.repository.LancamentoRepository;
@@ -23,7 +28,8 @@ public class LancamentoServiceImpl implements LancamentoService{
 	@Override
 	@Transactional
 	public Lancamento salvar(Lancamento lancamento) {
-		// TODO Auto-generated method stub
+		validar(lancamento);
+		lancamento.setStatus(StatusLancamento.PENDENTE);
 		return repository.save(lancamento);
 	}
 
@@ -31,6 +37,7 @@ public class LancamentoServiceImpl implements LancamentoService{
 	@Transactional
 	public Lancamento atualizar(Lancamento lancamento) {
 		Objects.requireNonNull(lancamento.getId());
+		validar(lancamento);
 		return repository.save(lancamento);
 	}
 
@@ -43,15 +50,50 @@ public class LancamentoServiceImpl implements LancamentoService{
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Example example = Example.of(lancamentoFiltro,
+				ExampleMatcher.matching()
+				.withIgnoreCase()
+				.withStringMatcher(StringMatcher.CONTAINING));
+		
+		return repository.findAll(example);
 	}
 
 	@Override
 	public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
 		lancamento.setStatus(status);
 		atualizar(lancamento);
+	}
+
+	@Override
+	public void validar(Lancamento lancamento) {
+		
+		if(lancamento.getDescricao() == null || lancamento.getDescricao().trim().equals("")) {
+			throw new RegraNegocioException("Informe uma Descrição valida.");
+		}
+		
+		if(lancamento.getMes() == null || lancamento.getMes() < 1 || lancamento.getMes() > 12) {
+			throw new RegraNegocioException("Informe um Mês válido.");
+		}
+		
+		if(lancamento.getAno() == null || lancamento.getAno().toString().length() != 4) {
+			throw new RegraNegocioException("Informe um Ano válido");
+		}
+		
+		if(lancamento.getUsuario() == null || lancamento.getUsuario().getId() == null) {
+			throw new RegraNegocioException("Informe um Usuário");
+		}
+		
+		if(lancamento.getValor() == null || lancamento.getValor().compareTo(BigDecimal.ZERO) < 1) {
+			throw new RegraNegocioException("Informe um Valor válido");
+		}
+		
+		if(lancamento.getTipo() == null) {
+			throw new RegraNegocioException("Informe um tipo de Lançamento");
+		}
+		
 	}
 
 }
